0769d508f676bf8e28259066a46bef186b9c6d6d
Neeraj <neeraj.manoj@anywhere.co>
2023-08-29 12:46:49 +0530

    added port

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,8 @@ module.exports = {
     devServer: {
         static: {
             directory: path.resolve(__dirname, './dist')
-        }
+        },
+        port: 3000
     },
     module: {
         rules: [
